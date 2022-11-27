0b2d3e4e0418b3f06fb34b6c74a16d127a2e8dae
gafaradetunji <gafaradetunji471@gmail.com>
2022-11-27 21:54:12 +0100

    executable

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,44 @@
-#!usr/bin/python3
-""" BaseModel class"""
-
-import uuid
-
+#!/usr/bin/python3
+"""
+This is the base model that contains serial/deserial information
+"""
 from datetime import datetime
-import models
-#datetime format
-dtm = "%Y-%m-%dT%H:%M:%S.%f"
+import uuid
+from models import storage
 
-class BaseModel:
-    """ 
-        Initializing the BaseModel...
-        Create a public instance attributes eg. id in string, created_at and updated_at
-    """
+
+class BaseModel():
+    """ Defines all common attributes/methods for other classes """
     def __init__(self, *args, **kwargs):
-
+        """ Initializes the instances attributes """
         if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-                elif "created_at" == key:
-                    self.created_at = datetime.strptime(kwargs["created_at"],dtm)
-                elif "updated_at" == key:
-                    self.updated_at = datetime.strptime(kwargs["updated_at"],dtm)
-                else:
-                    pass
-
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+            k_dict = kwargs.copy()
+            del k_dict["__class__"]
+            for key in k_dict:
+                if (key == "created_at" or key == "updated_at"):
+                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
+            self.__dict__ = k_dict
         else:
-            self.id = str(uuid.uuid4())  #Generates a Random id everytime an instance is created
-            date_info=datetime.now()
-            self.created_at = date_info
-            self.updated_at = date_info
-            models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            storage.new(self)
 
     def __str__(self):
-        """ should print: [<class name>] (<self.id>) <self.__dict__> """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """ Prints object in friendly format"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """ updates the public instance attribute updated_at with the current datetime """
-        self.updated_at = datetime.now()
-        models.storage.save()
+        """ Updates update_at """
+        self.updated_at = datetime.today()
+        storage.save()
 
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of __dict__ of the instance """
-        n_dict = self.__dict__.copy()
-        n_dict["created_at"] = self.created_at.isoformat()
-        n_dict["updated_at"] = self.updated_at.isoformat()
-        n_dict["__class__"] = self.__class__.__name__
-        return n_dict
+        """ Generate a new dict with an extra field __class__ """
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict

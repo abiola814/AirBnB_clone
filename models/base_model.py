2e4e027ab64fc26fa5fbee9642dec872f33a3aa7
Your Name <you@youraddress.com>
2022-11-27 11:54:08 +0100

    basemodel str adjust

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,7 @@ class BaseModel:
 
     def __str__(self):
         """ should print: [<class name>] (<self.id>) <self.__dict__> """
-        return self.__class__.__name__, self.id, self.__dict__
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime """
